0ee3eeda7fd07b26ce60686e7491ba4bac32b6f0
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-03-03 13:20:19 +0100

    Summary

---
+++ Kaffemaskin/Kaffemaskin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kaffemaskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
