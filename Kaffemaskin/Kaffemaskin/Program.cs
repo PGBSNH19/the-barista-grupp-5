753fe6c0fc06caff036b45c53c86d374e2e4226f
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-03-03 13:34:20 +0100

    @singelmamma

--- Kaffemaskin/Kaffemaskin/Program.cs
+++ Kaffemaskin/Kaffemaskin/Program.cs
@@ -8,5 +8,41 @@ namespace Kaffemaskin
         {
             Console.WriteLine("Hello World!");
         }
+        
+        public void Start()
+        {
+            var bean = new Bean();
+            var grinder = new Grinder();
+            var coffeMaker = new CoffeeMaker();
+            var cup = new Cup();
+        }
+}
+   
+    class Bean
+    {
+        string origin;
+        string roastLevel;
+        string beanType;
+        bool grind;
+    }
+
+    class Grinder
+    {
+        string grindSetting;
+    }
+
+    class CoffeeMaker
+    {
+        int strenght;
+        int waterVolume;
+        string filter;
+    }
+
+    class Cup
+    {
+        double cupVolume;
+        double milk;
+        bool sugar;
+
     }
 }
