075aff2a8a7037b15855e848b04e9a2a293d647f
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-03-05 14:12:34 +0100

    @andremorad

--- Kaffemaskin/Kaffemaskin/Program.cs
+++ Kaffemaskin/Kaffemaskin/Program.cs
@@ -5,7 +5,15 @@ namespace Coffe
 {
     public interface ICoffee
     {
+        List<string> ingredient { get
+            {
+                return
+            }
+            set
+            { 
 
+            }
+        }
         string CoffeeType { get; }
 
         ICoffee AddChocolateSyrup();
@@ -67,12 +75,6 @@ namespace Coffe
             public void CreateRecipies()
             {
 
-                Recpies.Add(new CoffeType() { coffeetype = "Cappuccino" }) ;
-                Recpies.Add(new CoffeType() { coffeetype = "Americano" });
-                Recpies.Add(new CoffeType() { coffeetype = "Espresso" });
-                Recpies.Add(new CoffeType() { coffeetype = "Macchiato" });
-                Recpies.Add(new CoffeType() { coffeetype = "Mocha" });
-                Recpies.Add(new CoffeType() { coffeetype = "Latte" });
 
             }
         }
@@ -80,12 +82,36 @@ namespace Coffe
         public class Ingredient
         {
             public string name;
+
+        
+
+        
         }
 
-        public class CoffeType
-        {
-            public string coffeetype;
-            List<Ingredient> Ingredients = new List<Ingredient>();
-        }
+        
+    class Cappuccino
+    {
+        List<Object> ingredient = new List<Object>();
+    }
+    class Americano
+    {
+
+    }
+    class Espresso
+    {
+
+    }
+    class Macchiato
+    {
+
+    }
+    class Mocha
+    {
+
+    }
+    class Latte
+    {
+
+    }
 }
 
